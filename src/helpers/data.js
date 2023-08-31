2e02db2a656bc7066ce704cfb7aeca7269799baf
axel-ac <abdullahceylan.axel@gmail.com>
2023-08-31 17:49:58 +0200

    start

---
+++ src/helpers/data.js
@@ -0,0 +1,82 @@
+export const data = [
+  {
+    name: "Hakan Sukur",
+    img: "https://www.cultkits.com/cdn/shop/articles/The_story_of_Hakan_Sukur.jpg?v=1644359873",
+    statistics: [
+      "1997 IFFHS World's Best Scorer award",
+      "1997 IFFHS World Top Scorer award",
+      "1997 Bronze Shoe award",
+      "1999-2000 European Cup Top Scorer: 10 goals",
+      "1998-1999 Turkish League Top Scorer: 19 goals",
+      "Top goal scorer in the world, Turkish football player: 395 goals",
+      "Top scorer in Turkish Football League history: 249 goals",
+      "Top scorer in the national team: 63 goals",
+      "Fastest goal scorer in FIFA World Cup history: 10.8 seconds (South Korea - 29.06.2002)",
+    ],
+  },
+  {
+    name: "Diego Maradona",
+    img: "https://upload.wikimedia.org/wikipedia/commons/4/48/Argentina_celebrando_copa_%28cropped%29.jpg",
+    statistics: [
+      "Napoli all-time Top Scorer",
+      "FIFA Player of the Century: 2000",
+      "FIFA World Cup Goal of the Century (1986 (2-1) v. England; second goal): 2002",
+      "FIFA 100 Best Living Players: 2004",
+      "Primera Division Argentine top scorer (5)",
+      "FIFA World Youth Championship Golden Ball: 1979",
+      "World Player of the Year (Guerin Sportivo): 1979",
+      "FIFA World Cup Assist King: 1986",
+      "Top 100 Footballers of the 20th Century: (#2) 1999",
+    ],
+  },
+  {
+    name: "Pelé",
+    img: "https://i0.wp.com/penceremden.com/wp-content/uploads/2022/12/Pele.png?fit=1424%2C800&ssl=1",
+    statistics: ["-"],
+  },
+  {
+    name: "Zinédine Zidane",
+    img: "https://www.denverpost.com/wp-content/uploads/2016/05/20060706_122826_sp06worldcup1.jpg?w=400",
+    statistics: ["-"],
+  },
+  {
+    name: "Andres Iniesta",
+    img: "https://cdn.images.express.co.uk/img/dynamic/67/590x/secondary/Barcelona-Andres-Iniesta-Champions-League-Real-Madrid-1814214.jpg?r=1554494859019",
+    statistics: ["-"],
+  },
+  {
+    name: "Thomas Müller",
+    img: "https://cdn.vox-cdn.com/thumbor/S2y7ZQGfxb3go_X8MLxA8DvW8_Q=/0x0:2053x3000/1200x800/filters:focal(1054x913:1382x1241)/cdn.vox-cdn.com/uploads/chorus_image/image/66090787/452119012.jpg.0.jpg",
+    statistics: ["-"],
+  },
+  {
+    name: "Lionel Messi",
+    img: "https://cdnuploads.aa.com.tr/uploads/Contents/2023/02/21/thumbs_b_c_2d64b147d001ddb86e23a382ea66bc4b.jpg?v=143423",
+    statistics: ["-"],
+  },
+  {
+    name: "Cristiano Ronaldo",
+    img: "https://s.iw.ro/gateway/g/ZmlsZVNvdXJjZT1odHRwJTNBJTJGJTJG/c3RvcmFnZTAxZGlnaXMucmNzLXJkcy5y/byUyRnN0b3JhZ2UlMkYyMDIzJTJGMDUl/MkYwMyUyRjE3MjM0OTRfMTcyMzQ5NF9w/cm9maW1lZGlhLTAzNTczNTU2NjIuanBn/Jnc9NzAwJmg9NDIwJmhhc2g9YzBmOTU5/M2E0Mzg0OTBmZTgyN2ExY2JiNDM3ZTRhODE=.thumb.jpg",
+    statistics: ["-"],
+  },
+  {
+    name: "Roberto Carlos",
+    img: "https://img.piri.net/mnresize/900/-/resim/upload/2018/03/01/02/44/d2a8da34bef51114d6a953834c379f5bdb6e47e7.jpg",
+    statistics: ["-"],
+  },
+  {
+    name: "Paolo Maldini",
+    img: "https://yasamoykusu.com/_docs/story/Paolo_Maldini3_yasamoykusu.jpg",
+    statistics: ["-"],
+  },
+  {
+    name: "Zlatan Ibrahimovic",
+    img: "https://i.tmgrup.com.tr/gq/img/920x615/23-02/05/zlatan-ibramovic-hm-gq-1675589348.jpg?0.4629854768291889",
+    statistics: ["-"],
+  },
+  {
+    name: "Franck Ribery",
+    img: "https://s.bundesliga.com/assets/img/1190000/1185769_imgw968.jpg",
+    statistics: ["-"],
+  },
+];
