98af68cf2a80805cf24cbe6647a5b1832b2bf20a
axel-ac <abdullahceylan.axel@gmail.com>
2023-08-31 23:31:16 +0200

    card-container

---
+++ src/components/PlayerCard.jsx
@@ -0,0 +1,15 @@
+import Card from "react-bootstrap/Card";
+
+
+const PlayerCard = ({ name, img, statistics }) => {
+  return (
+    <Card className="rounded-2 m-auto player-card" role="button">
+      <Card.Img variant="top" src={img} className="player-logo" />
+      <Card.Footer>
+        <Card.Title>{name}</Card.Title>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default PlayerCard
